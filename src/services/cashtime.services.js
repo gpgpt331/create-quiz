92688b6dd4d7935e273746b3dea02d59be44ac4a
gpgpt331 <gpgpt64@gmail.com>
2024-09-02 10:31:09 -0300

    cashtime-services

--- src/services/cashtime.services.js
+++ src/services/cashtime.services.js
@@ -1,66 +1,65 @@
 import axios from "axios";
 const CASHTIME_SK = "sk_live_DwK6Yt9z1WSLkkIpoJYiyHG9LE5MJiThFPMPsWJipX"
 
-
 const cashtimeservices = async (
-  userId,
-  userName,
-  userEmail,
-  planId,
-  planName,
-  amount) => {
-  try {
-      const { data: transaction } = await axios.post(
-          "https://api.sandbox.hopysplit.com.br/v1/transfers",
-          {
-              postbackUrl: "https://adequate-amazement-production.up.railway.app/api/webhooks/subscribe",
-              amount: parseFloat(amount) * 100 || 0,
-              paymentMethod: "pix",
-              metadata: {
-                  userId,
-                  planId
-              },
-              customer: {
-                  name: userName,
-                  email: userEmail,
-                  document: {
-                      type: "cpf",
-                      number: "" // Assumindo que o número de CPF será preenchido aqui
-                  }
-              },
-              items: [
-                  {
-                      title: planName || `Plano de Assinatura`,
-                      unitPrice: parseFloat(amount) * 100 || 0,
-                      quantity: 1,
-                      tangible: false
-                  }
-              ]
-          },
-          {
-              headers: {
-                  accept: "application/json",
-                  "content-type": "application/json",
-                  authorization:
-                      "Basic " + Buffer.from(`${CASHTIME_SK}:x`).toString("base64")
-              }
-          }
-      );
-
-      // Verifica se a transação retornou com sucesso e possui os dados do PIX
-      if (transaction.status === 'processing' || transaction.status === 'waiting_payment') {
-          return { qrcode: transaction.pix.qrcode };
-      } else {
-          throw new Error(`Transação falhou com status: ${transaction.status}`);
-      }
-
-  }   catch (error) {
-    // Captura e exibe a resposta completa do erro para debugging
-    console.log("Erro na requisição para Cashtime:", error.response?.data || error.message);
-    throw new Error("failed to fetch qrcode");
-}
-};
-
-
-
-export default cashtimeservices;
+    userId,
+    userName,
+    userEmail,
+    planId,
+    planName,
+    amount) => {
+    try {
+        const { data: transaction } = await axios.post(
+            "https://api.sandbox.hopysplit.com.br/v1/transfers",
+            {
+                postbackUrl: "https://adequate-amazement-production.up.railway.app/api/webhooks/subscribe",
+                amount: parseFloat(amount) * 100 || 0,
+                paymentMethod: "pix",
+                metadata: {
+                    userId,
+                    planId
+                },
+                customer: {
+                    name: userName,
+                    email: userEmail,
+                    document: {
+                        type: "cpf",
+                        number: "12345678900" // Substitua por um CPF de teste válido
+                    }
+                },
+                items: [
+                    {
+                        title: planName || `Plano de Assinatura`,
+                        unitPrice: parseFloat(amount) * 100 || 0,
+                        quantity: 1,
+                        tangible: false
+                    }
+                ]
+            },
+            {
+                headers: {
+                    accept: "application/json",
+                    "content-type": "application/json",
+                    authorization:
+                        "Basic " + Buffer.from(`${CASHTIME_SK}:x`).toString("base64")
+                }
+            }
+        );
+  
+        console.log("Resposta da Transação:", transaction);
+  
+        // Verifica se a transação retornou com sucesso e possui os dados do PIX
+        if (transaction.status === 'processing' || transaction.status === 'waiting_payment') {
+            return { qrcode: transaction.pix.qrcode };
+        } else {
+            throw new Error(`Transação falhou com status: ${transaction.status}`);
+        }
+  
+    } catch (error) {
+        console.log("Erro na requisição para Cashtime:", error.response?.data || error.message);
+        throw new Error(`Falha ao buscar o QR code: ${error.response?.data?.message || error.message}`);
+    }
+  };
+  
+  export default cashtimeservices;
+  
