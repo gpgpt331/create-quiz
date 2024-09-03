66c659f2400d4cc7dcbe321133b76de073cff95d
gpgpt331 <gpgpt64@gmail.com>
2024-09-03 17:04:56 -0300

    api upload

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -9,7 +9,7 @@ const ImageGallery = () => {
     const fetchImages = async () => {
       try {
         const token = localStorage.getItem('token'); // Pega o token de autenticação armazenado
-        const response = await axios.get(`${API_URL}/my-images`, {
+        const response = await axios.get(`${API_URL}/api/my-images`, {
           headers: {
             Authorization: `Bearer ${token}` // Envia o token no cabeçalho
           }
